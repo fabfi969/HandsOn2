1d8a8e1dfb9e266df767ec194216cd1761cb3b3d
Fabian <fabfi969@student.liu.se>
2024-09-23 17:37:14 +0200

    Forgot to commit change

--- md.py
+++ md.py
@@ -6,42 +6,52 @@ from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
 from asap3 import Trajectory
 
-# Use Asap for a huge performance increase if it is installed
-use_asap = True
+def calcenergy(a):  # store a reference to atoms in the definition.
+        """Function to calculate the potential, kinetic and total energy."""
+        epot = a.get_potential_energy() / len(a)
+        ekin = a.get_kinetic_energy() / len(a)
+        etot = epot + ekin
+        return(epot, ekin, etot)
 
-if use_asap:
-    from asap3 import EMT
-    size = 10
-else:
-    from ase.calculators.emt import EMT
-    size = 3
+def run_md():
+    # Use Asap for a huge performance increase if it is installed
+    use_asap = True
 
-# Set up a crystal
-atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                          symbol="Cu",
-                          size=(size, size, size),
-                          pbc=True)
+    if use_asap:
+        from asap3 import EMT
+        size = 10
+    else:
+        from ase.calculators.emt import EMT
+        size = 3
 
-# Describe the interatomic interactions with the Effective Medium Theory
-atoms.calc = EMT()
+    # Set up a crystal
+    atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                              symbol="Cu",
+                              size=(size, size, size),
+                              pbc=True)
 
-# Set the momenta corresponding to T=300K
-MaxwellBoltzmannDistribution(atoms, temperature_K=300)
+    # Describe the interatomic interactions with the Effective Medium Theory
+    atoms.calc = EMT()
 
-# We want to run MD with constant energy using the VelocityVerlet algorithm.
-dyn = VelocityVerlet(atoms, 1 * units.fs)  # 5 fs time step.
-traj = Trajectory("cu.traj", "w", atoms)
-dyn.attach(traj.write, interval=10)
+    # Set the momenta corresponding to T=300K
+    MaxwellBoltzmannDistribution(atoms, temperature_K=300)
 
-def printenergy(a=atoms):  # store a reference to atoms in the definition.
-    """Function to print the potential, kinetic and total energy."""
-    epot = a.get_potential_energy() / len(a)
-    ekin = a.get_kinetic_energy() / len(a)
-    print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-          'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
+    # We want to run MD with constant energy using the VelocityVerlet algorithm.
+    dyn = VelocityVerlet(atoms, 1 * units.fs)  # 5 fs time step.
+    traj = Trajectory("cu.traj", "w", atoms)
+    dyn.attach(traj.write, interval=10)
+
+    def printenergy(a=atoms):  # store a reference to atoms in the definition.
+        """Function to print the potential, kinetic and total energy."""
+        epot, ekin, etot = calcenergy(a)
+        print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
+              'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), etot))
 
 
-# Now run the dynamics
-dyn.attach(printenergy, interval=10)
-printenergy()
-dyn.run(1000)
+    # Now run the dynamics
+    dyn.attach(printenergy, interval=10)
+    printenergy()
+    dyn.run(1000)
+    
+if __name__ == "__main__":
+    run_md()
